6fa305871f7a29437ea2890ea54d501e7dcf4542
kabane <kabaneshi@gmail.com>
2022-06-30 05:50:52 +0900

    change default port number

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
+const port = 8888;
 
-const server = app.listen(3001, function(){
+const server = app.listen(8888, function(){
   console.log("Node.js is listening to PORT:" + server.address().port);
 });
 app.use(cors());
 app.use(express.json());
+
 
 const messages = [
   {
