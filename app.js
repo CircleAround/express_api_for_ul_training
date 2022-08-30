82c79f172dcd2db1bd4ff5059b1fd9f0c58352cb
kabane <kabaneshi@gmail.com>
2022-08-30 12:19:14 +0900

    応用課題用のエンドポイント追加

--- app.js
+++ app.js
@@ -30,6 +30,25 @@ const todos = [
   {id: 3, text: 'task3', done: false }
 ]
 
+const customers = [
+  { "id": 1,"name": "鈴木太郎", "gender": "男", "birthday": "1990/02/18", "organizationId": 1 },
+  { "id": 2, "name": "田中次郎", "gender": "男", "birthday": "1996/12/08", "organizationId": 1 },
+  { "id": 3, "name": "町田真美", "gender": "女", "birthday": "1993/05/11", "organizationId": 1 },
+  { "id": 4, "name": "大田三郎", "gender": "男", "birthday": "1991/08/13", "organizationId": 1 },
+  { "id": 5, "name": "松井花子", "gender": "女", "birthday": "1988/12/18", "organizationId": 2 },
+  { "id": 6, "name": "村田四郎", "gender": "男", "birthday": "1998/07/12", "organizationId": 2 },
+  { "id": 7, "name": "清田加奈子", "gender": "女", "birthday": "1997/06/19", "organizationId": 2 },
+  { "id": 8, "name": "松井京子", "gender": "女", "birthday": "1992/06/19", "organizationId": 3 },
+  { "id": 9, "name": "斎藤久美子", "gender": "女", "birthday": "1992/09/01", "organizationId": 3 },
+  { "id": 10, "name": "松本五郎", "gender": "男", "birthday": "1987/01/27", "organizationId": 3 }
+];
+
+const organizations = [
+  {　"id": 1,　"name": "A株式会社"　},
+  {　"id": 2,　"name": "B商事(株)"　},
+  {　"id": 3,　"name": "株式会社C"　}
+];
+
 app.get("/", (req, res) => {
   res.status(200).send("Express!!");
 });
@@ -53,4 +72,12 @@ app.post('/api/todos', function (req, res, next) {
   const data = { id: todos.length + 1, text: req.body.text, done: false  }
   todos.push(data)
   res.json(data)
+});
+
+app.get("/api/customers", function(req, res, next){
+  res.json(customers);
+});
+
+app.get("/api/organizations", function(req, res, next){
+  res.json(organizations);
 });
